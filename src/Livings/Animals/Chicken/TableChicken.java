2207d7443c4cb67dcdd5f0777dce903d1c20c7e3
hangshuaibi <1440620378@qq.com>
2019-10-25 01:29:16 +0800

    Add Iterator & Singleton

--- src/Livings/Animals/Chicken/TableChicken.java
+++ src/Livings/Animals/Chicken/TableChicken.java
@@ -4,6 +4,9 @@ package Livings.Animals.Chicken;
  * 肉鸡类
  */
 public class TableChicken extends Chicken{
+    public TableChicken(){
+        super.setName("TableChicken");
+    }
     @Override
     public void gobble(){
         System.out.println("Table Children!");
