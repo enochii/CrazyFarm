af85fcb5ee5d536a5d76802f96ddc394b8fac638
jianghua <tianfangjh@gmail.com>
2019-10-26 23:42:05 +0800

    add mediator

--- src/mediator/AnimalMediator.java
+++ src/mediator/AnimalMediator.java
@@ -6,16 +6,25 @@ import Livings.Animals.Animal;
 
 public class AnimalMediator implements Mediator{
 
-    private Menu<Animal> AnimalMenu;
-    private Farm farm;
+    private Menu<Animal> _animalMenu;
+    private Farm _farm;
 
-    public void createColleagues(Farm farm){
-        this.farm = farm;
+    public void createColleagues(){
+        this._farm = Farm.getInstance();
+
+
     }
 
     public void colleagueChanged(){
 
     }
+
+    public AnimalMediator(){
+        createColleagues();
+        System.out.println("AnimalMediator has been initialized");
+    }
+
+
 
 
 
--- src/mediator/Mediator.java
+++ src/mediator/Mediator.java
@@ -5,6 +5,6 @@ import Farm.Farm;
 import java.io.Serializable;
 
 public interface Mediator {
-    public abstract void createColleagues(Farm farm);
-    public abstract void ColleagueChanged();
+    public abstract void createColleagues();
+    public abstract void colleagueChanged();
 }
