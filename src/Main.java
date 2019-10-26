1b83a158fb6b74f8a25d2e53e32ac5e57d4377a1
hangshuaibi <1440620378@qq.com>
2019-10-26 19:43:59 +0800

    Add lib support for JUnit & test for interpreter

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@ import Adapter.ChickenAdapter;
 import Farm.Menu;
 import Interpreter.Parser;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import junit.framework.JUnit4TestAdapter;
+
 public class Main {
 
     public static void main(String[] args) {
