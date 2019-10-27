feafb21a2fc1d9288a2c22c84172939f0a06bdd2
jianghua <tianfangjh@gmail.com>
2019-10-27 13:49:04 +0800

    complete state

--- src/State/Context.java
+++ src/State/Context.java
@@ -2,7 +2,7 @@ package State;
 
 public interface Context {
 
-    public abstract void setClock(int hour);
+    public abstract void setClock(int currentTime);
     public abstract void changeState(State state);
     public abstract void getFed(int hour);
 
