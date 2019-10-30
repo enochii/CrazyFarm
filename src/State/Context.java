b742290dd46d62f37794fadea0b4cce3f31c70f3
jianghua <tianfangjh@gmail.com>
2019-10-30 22:49:54 +0800

    modify some code

--- src/State/Context.java
+++ src/State/Context.java
@@ -1,10 +1,30 @@
 package State;
 
+/**
+ * The interface Context.
+ */
 public interface Context {
 
 
+    /**
+     * Sets clock.
+     *
+     * @param currentTime the current time
+     */
     public abstract void setClock(int currentTime);
+
+    /**
+     * Change state.
+     *
+     * @param state the state
+     */
     public abstract void changeState(State state);
+
+    /**
+     * Gets fed.
+     *
+     * @param currentTime the current time
+     */
     public abstract void getFed(int currentTime);
 
 }
--- src/State/State.java
+++ src/State/State.java
@@ -2,10 +2,30 @@ package State;
 
 import Livings.Living;
 
+/**
+ * The interface State.
+ */
 public interface State {
 
+    /**
+     * Gets full state.
+     *
+     * @param context the context
+     */
     public void getFullState(Context context);
+
+    /**
+     * Gets hungry state.
+     *
+     * @param context the context
+     */
     public void getHungryState(Context context);
+
+    /**
+     * Gain experience.
+     *
+     * @param living the living
+     */
     public void gainExperience(Living living);
 
 
