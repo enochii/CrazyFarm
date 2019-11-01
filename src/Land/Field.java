0ba62ec64bf7f107e22547dcaafbee026ef239f3
Leon <leonwangchn@163.com>
2019-11-01 15:53:49 +0800

    perfect factory pattern

--- src/Land/Field.java
+++ src/Land/Field.java
@@ -1,8 +1,17 @@
 package Land;
 
 public class Field extends Land {
+    String _usage;
+
     public Field() { _usage=""; }
     public Field(String usage) {
         this._usage = usage;
+    }
+
+    public String getUsage() { return _usage; }
+
+    @Override
+    public void use() {
+        System.out.println("This field is used.");
     }
 }
