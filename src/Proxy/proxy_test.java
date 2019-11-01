ed79042027f7d00283b32ea2c705a27679245c5f
Chao Zheng <chaoszh@foxmail.com>
2019-11-02 02:15:00 +0800

    proxy with bug.

---
+++ src/Proxy/proxy_test.java
@@ -0,0 +1,14 @@
+package Proxy;
+
+
+public class proxy_test {
+	public static void main(String[] args) {
+		OwnerProxy ownerProxy = new OwnerProxy();
+		System.out.println("测试代理获取owner的财产。");
+		ownerProxy.getMoney();
+		System.out.println("测试owner通过代理花钱，且钱不够的情况。");
+		ownerProxy.reduceMoney(1300);
+		System.out.println("测试owner通过代理花钱，钱足够的情况。");
+		ownerProxy.reduceMoney(300);
+	}
+}
