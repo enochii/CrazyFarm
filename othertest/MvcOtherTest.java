9455b5fd4e1b79487f9c111bf821a9788a03d5f5
jianghua <tianfangjh@gmail.com>
2019-11-11 19:57:48 +0800

    add other test

---
+++ othertest/MvcOtherTest.java
@@ -0,0 +1,23 @@
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @program: crazyfarm->MvcOtherTest
+ * @description:
+ * @create: 2019-11-11 19:54
+ **/
+
+public class MvcOtherTest {
+
+    @Before
+    public void init()
+    {
+        System.out.println("mvctest");
+    }
+
+    @Test
+    public void mvcOtherTest()
+    {
+        System.out.println("test has finished");
+    }
+}
